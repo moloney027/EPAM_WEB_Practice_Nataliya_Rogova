48a4d0baf00cfe49f3a59908266687716d3cd272
averyroof <rogova.nataliya1999@mail.ru>
2020-07-02 17:19:52 +0400

    fixed sass, add clicker

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+let count = 0;
+function functionClick() {
+  count+=1;
+  console.log("Click");
+  document.getElementById("cl").innerHTML = count;
+ }
+
+ document.getElementsByTagName("body")[0].onclick = functionClick
