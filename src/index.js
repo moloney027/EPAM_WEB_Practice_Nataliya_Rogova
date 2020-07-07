c1719eab6cce08408c43f3d1aac5ef29c85735ea
averyroof <rogova.nataliya1999@mail.ru>
2020-07-07 21:11:13 +0400

    client is connected to the server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import "./js/common";
+import "./js/client";
+import "./js/server";
 import "./scss/main.scss";
+const client = require("./js/client.js");
---
+++ src/js/client.js
@@ -0,0 +1,5 @@
+var io = require("socket.io-client");
+var socket = io.connect("https://voicy-speaker.herokuapp.com/");
+socket.on("connect", function () {
+  console.log("Client has connected to the server!");
+});
