d8bb4daa50caa5383e6e7be7ab786a25902485c7
averyroof <rogova.nataliya1999@mail.ru>
2020-07-08 20:32:09 +0400

    added recording and sending messages

--- src/js/client.js
+++ src/js/client.js
@@ -3,3 +3,4 @@ var socket = io.connect("https://voicy-speaker.herokuapp.com/");
 socket.on("connect", function () {
   console.log("Client has connected to the server!");
 });
+module.exports = { socket: socket };
--- src/js/common.js
+++ src/js/common.js
@@ -1,20 +1,17 @@
-import "./client";
-const socket = require("./client.js");
+const { socket } = require("./client.js");
 
 function action1() {
-  (response = document.getElementById("response")),
-    (output = "Active: voices list");
+  var response = document.getElementById("response");
+  var output = "Active: voices list";
   response.innerHTML = output;
 }
 document.getElementById("bar").onclick = action1;
 
 function action2() {
-  // отображение режима speaker-mode
-  (response = document.getElementById("response")),
-    (output = "Active: speaker-mode");
+  var response = document.getElementById("response");
+  var output = "Active: speaker-mode";
   response.innerHTML = output;
 
-  // запись и отправка голосового сообщения
   var constraints = { audio: true };
   navigator.mediaDevices.getUserMedia(constraints).then(function (mediaStream) {
     const mediaRecorder = new MediaRecorder(mediaStream);
@@ -26,30 +23,10 @@ function action2() {
 
     mediaRecorder.start();
 
-    // завершить через 3 секунды
     setTimeout(function () {
       mediaRecorder.stop();
       console.log("Запись завершена!");
     }, 3000);
-
-    // другой вариант
-    // mediaRecorder.onstart = function (e) {
-    //   this.chunks = [];
-    // };
-    // mediaRecorder.ondataavailable = function (e) {
-    //   this.chunks.push(e.data);
-    // };
-    // mediaRecorder.onstop = function (e) {
-    //   var audioChunks = new Blob(this.chunks, {
-    //     type: "audio/ogg; codecs=opus",
-    //   });
-    //   socket.emit("audioMessage", audioChunks);
-    // };
-    // mediaRecorder.start();
-    // setTimeout(function () {
-    //   mediaRecorder.stop();
-    //   console.log("Запись завершена!");
-    // }, 3000);
   });
 }
 document.getElementById("micr").onclick = action2;
